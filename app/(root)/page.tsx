c6784132a110ef62869b1b8983b77397191aa94e
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-01-27 21:28:21 +0530

    ADD: Code for the Hero Section of the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup, <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Build Projects, Submit Ideas and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
