f508e0bd88de47948d2a54e1cf67588615a89420
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-01-27 22:57:48 +0530

    ADD: UI Component for Startup detail Card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,6 +7,20 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: Date.now(),
+      views: 55,
+      author: { _id: 1, name: "Adriano" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://images.pexels.com/photos/2085832/pexels-photo-2085832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -16,6 +31,22 @@ export default async function Home({
           Build Projects, Submit Ideas and Get Noticed in Virtual Competitions
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}" : ` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-result">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
